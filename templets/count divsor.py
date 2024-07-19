09225bfa947933c013b5d8154a2ff4da41073dc3
Mahmoud khaled <162627248+Mahmoud-kh1@users.noreply.github.com>
2024-07-19 22:18:46 +0300

    Update count divsor.py

--- templets/count divsor.py
+++ templets/count divsor.py
@@ -1,3 +1,5 @@
+// count divisor in n^(1 / 3)
+
 N = input()
 primes = array containing primes till 10^6
 ans = 1
@@ -15,3 +17,19 @@ else if N is square of a prime:
             ans = ans * 3
 else if N != 1:
             ans = ans * 4v
+
+sum propeerts of GCD 
+ gcd(a, lcm(b, c)) = lcm(gcd(a, b), gcd(a, c))
+
+lcm(a, gcd(b, c)) = gcd(lcm(a, b), lcm(a, c)).
+
+gcd(n^a-1,n^b-1)=n^gcd(a,b)-1 
+
+
+// submask
+for (int mask = 1;  mask < constrins; maks++){
+for (int submask = mask; submask; submask = (submask - 1) & mask) {
+        submasks.push_back(submask);
+    }
+}
+time o(3 ^ n)
