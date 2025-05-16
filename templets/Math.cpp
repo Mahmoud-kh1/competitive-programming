9c441284a2fafc35afe83bd7d0fdc093473f8966
Mahmoud khaled <162627248+Mahmoud-kh1@users.noreply.github.com>
2025-05-16 05:11:09 +0300

    Update Math.cpp

--- templets/Math.cpp
+++ templets/Math.cpp
@@ -1,6 +1,6 @@
 search for all version for solve_diophantine equation 
 all templets that do magic things 
 
-  fib rules 
+  fib rules  spically shift by pos, and negative 
 
 gcd(fib[a] , fib[b]) = fib[gcd(a,b)] 
