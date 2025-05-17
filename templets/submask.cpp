3a0a18e2f200ee2e8965e5a4a70fdfd46a7ae8d4
Mahmoud khaled <162627248+Mahmoud-kh1@users.noreply.github.com>
2025-05-17 22:51:05 +0300

    Update submask.cpp

--- templets/submask.cpp
+++ templets/submask.cpp
@@ -1,4 +1,4 @@
-
+dont forget to get all function of bitset it's important for dp problems
 for (int mask = 1;  mask < constrins; maks++){
 for (int submask = mask; submask; submask = (submask - 1) & mask) {
         submasks.push_back(submask);
