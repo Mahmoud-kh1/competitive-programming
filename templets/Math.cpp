df415896649920b50efe79c4e60852af1a6d32c5
Mahmoud khaled <162627248+Mahmoud-kh1@users.noreply.github.com>
2025-05-23 03:00:45 +0300

    Update Math.cpp

--- templets/Math.cpp
+++ templets/Math.cpp
@@ -4,3 +4,6 @@ all templets that do magic things
   fib rules  spically shift by pos, and negative 
 
 gcd(fib[a] , fib[b]) = fib[gcd(a,b)] 
+
+
+https://cp-algorithms.com/algebra/phi-function.html  important for (a^b) mod m 
