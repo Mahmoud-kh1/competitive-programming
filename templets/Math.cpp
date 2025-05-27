6439e3de75fc34e36b50b84738ba11fe36a90043
Mahmoud khaled <162627248+Mahmoud-kh1@users.noreply.github.com>
2025-05-27 08:29:22 +0300

    Update Math.cpp

--- templets/Math.cpp
+++ templets/Math.cpp
@@ -7,3 +7,19 @@ gcd(fib[a] , fib[b]) = fib[gcd(a,b)]
 
 
 https://cp-algorithms.com/algebra/phi-function.html  important for (a^b) mod m 
+
+
+
+long long safe_mul(long long a, long long b, long long mod) {
+    a %= mod;
+    b %= mod;
+    long long result = 0;
+    while (b > 0) {
+        if (b & 1) {
+            result = (result + a) % mod;
+        }
+        a = (a + a) % mod;
+        b >>= 1;
+    }
+    return result;
+} safe mul to compute (a * b) % mod without overflow 
