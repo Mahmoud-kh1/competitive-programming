2a9a00771d4a7780b83bafea5a2be2c204dcc7b0
Mahmoud khaled <162627248+Mahmoud-kh1@users.noreply.github.com>
2025-07-28 18:43:07 +0300

    Update binary trie.cpp

--- templets/DataStructure/binary Trie/binary trie.cpp
+++ templets/DataStructure/binary Trie/binary trie.cpp
@@ -1,5 +1,6 @@
 #include "bits/stdc++.h"
 using namespace std;
+#define int long long
 struct Node{
     int ch[2]{};
     int sz = 0;
@@ -8,8 +9,7 @@ struct Node{
         return ch[x];
     }
 };
-// change if needed 
-int M = 31;
+int M = 50;
 struct Trie{
     vector<Node>trie;
 
@@ -38,12 +38,12 @@ struct Trie{
         int cur = 0;
         long long ans = 0;
         for(int i = M - 1; i >= 0; i--){
-            int preBit = (x >> i) & 1;
+            int xbit = (x >> i) & 1;
             int kBit = (k >> i) & 1;
-            if (preBit == 0 && kBit == 0) ans += trie[trie[cur][1]].sz;
-            if (preBit == 1 && kBit == 0) ans += trie[trie[cur][0]].sz;
-            if (trie[cur][preBit ^ kBit]){
-                cur = trie[cur][preBit ^ kBit];
+            if (xbit == 0 && kBit == 0) ans += trie[trie[cur][1]].sz;
+            if (xbit == 1 && kBit == 0) ans += trie[trie[cur][0]].sz;
+            if (trie[cur][xbit ^ kBit]){
+                cur = trie[cur][xbit ^ kBit];
             }
             else break;
 
@@ -51,9 +51,26 @@ struct Trie{
         }
         return ans + trie[cur].isEnd;
     }
+    // count x^a[i] <= k
+    long long get2(int x, int k){
+        int cur = 0;
+        long long ans = 0;
+        for(int i = M - 1; i >= 0; i--){
+            int xbit = (x >> i) & 1;
+            int kBit = (k >> i) & 1;
+            if (xbit == 0 && kBit == 1) ans += trie[trie[cur][0]].sz;
+            if (xbit == 1 && kBit == 1) ans += trie[trie[cur][1]].sz;
+            if (trie[cur][xbit ^ kBit]){
+                cur = trie[cur][xbit ^ kBit];
+            }
+            else break;
 
+
+        }
+        return ans + trie[cur].isEnd;
+    }
     // get max xor with (x)
-    long long get2(int x) {
+    long long get3(int x) {
         int cur = 0;
         int  ans = 0;
         for(int i = M - 1; i >= 0; i--){
@@ -77,19 +94,41 @@ void fast() {
 }
 signed  main() {
     fast();
-    int n, k; cin >> n >> k;
-    vector<int>a(n);
-    for(int i = 0; i < n; i++) cin >> a[i];
-    for(int i = 1; i < n; i++) a[i]^= a[i - 1];
-    Trie trie;
-    trie.update(0, 1);
-    long long  final = 0;
-    for(int i = 0;  i < n; i++){
-        final += trie.get1(a[i], k);
-        trie.update(a[i], 1);
-    }
-    cout << final << endl;
+    int t = 1; cin >> t;
+    while(t--) {
+        int n, k;
+        cin >> n >> k;
+        int a[n];
+        for (int i = 0; i < n; i++) cin >> a[i];
+        Trie trie;
 
+
+        auto can = [&](int mid) {
+            int cntPairs = 0;
+            trie.init();
+            int l = 0, r = 0, res = 0;
+            while (r < n) {
+                cntPairs += trie.get2(a[r], mid);
+                trie.update(a[r], 1);
+                while (cntPairs > 0) {
+                    trie.update(a[l], -1);
+                    cntPairs -= trie.get2(a[l], mid);
+                    l++;
+                }
+                res += l;
+                r++;
+            }
+            return res;
+        };
+        int L = 0, R =3e10, ans = 3e10;
+        while (L <= R) {
+            int mid = (L + R) / 2;
+            int cnt = can(mid);
+            if (cnt >= k) ans = mid, R = mid - 1;
+            else L = mid + 1;
+        }
+        cout << ans << endl;;
+    }
 
 
 
